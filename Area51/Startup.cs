e6646e537ccf08e398b740b8b92b76b555e149e0
dushyantgill <dushyantgill@gmail.com>
2015-03-29 02:07:46 -0700

    VipSwapper

---
+++ Area51/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Area51.Startup))]
+namespace Area51
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
